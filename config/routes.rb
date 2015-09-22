23fe4b2e0fd9d2e34dd19545f836e807fc4eb6e0
MaxMillington <maxmillington89@gmail.com>
2015-09-22 09:00:59 -0600

    moved nav bar into partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get "numbers/new",  to: "numbers#new"
+  get "locations", to: "locations#index"
+  get "locations/new", to: "locations#new"
+
 end
