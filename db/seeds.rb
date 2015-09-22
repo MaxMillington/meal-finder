3f282f86aaf6d959b6bd8dc6b028e8d0e737f557
MaxMillington <maxmillington89@gmail.com>
2015-09-22 14:59:49 -0600

    added some seeds of some charities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Location.create(name: "Project Angel Heart", address: "4950 Washington St. Denver, CO 80216")
+Location.create(name: "Samaritan House Catholic Charities", address: "2301 Lawrence St, Denver, CO 80205")
+Location.create(name: "Denver Rescue Mission", address: "1130 Park Ave W, Denver, CO 80205")
