867e2440545e75ff560435c1b0d51cc5ab2107b8
MaxMillington <maxmillington89@gmail.com>
2015-09-22 15:46:47 -0600

    added markers and infowindow to map

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,14 @@ class LocationsController < ApplicationController
   def index
     def index
       @locations = Location.all
-      @json = @locations.to_gmaps4rails
+      @json = @locations.to_gmaps4rails do |location, marker|
+        marker.infowindow render_to_string(:partial => "/locations/infowindow", :locals => { :location => location})
+        marker.title "#{location.name}"
+        marker.json({ :address => location.address})
+        # marker.picture({:picture => "http://mapicons.nicolasmollet.com/     wp-content/uploads/mapicons/shape-default/color-3875d7/shapeco     lor-color/shadow-1/border-dark/symbolstyle-contrast/symbolshad     owstyle-dark/gradient-iphone/information.png",
+        #                 :width => 32,
+        #                 :height => 32})
+      end
     end
   end
 
