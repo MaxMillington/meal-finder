b4e633f1a6d7e7c89b0b81a65a7e303173289a7b
MaxMillington <maxmillington89@gmail.com>
2015-09-23 10:59:34 -0600

    added empty admin dashboard view, fixed an error in the controller, and fixed the logout button for the admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_action :require_login
 
   def require_login
-    render file: "/public/404" unless current_user?
+    render file: "/public/404" unless current_user
   end
 
   def index
