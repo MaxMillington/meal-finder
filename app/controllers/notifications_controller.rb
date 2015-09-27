d006de7ad5b2e387a55787cec0c4e32f15ea7138
MaxMillington <maxmillington89@gmail.com>
2015-09-27 17:14:26 -0600

    made it so that the text messages now show the location information

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,10 +8,16 @@ class NotificationsController < ApplicationController
     @numbers = Number.all
     client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
                                       ENV["twilio_auth_token"])
+    @locations = Location.all
+
+    message = @locations.map do |location|
+      "#{location.name}: #{location.description}"
+    end
+    
     @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "1#{number.phone_number.delete '-'}",
-                             body: "Hello")
+        client.messages.create(from: "15702854146",
+                               to: "1#{number.phone_number.delete '-'}",
+                               body: "#{message.join(', ')}")
     end
     redirect_to admin_dashboard_path
   end
