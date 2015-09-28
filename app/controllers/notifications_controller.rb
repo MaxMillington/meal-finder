818d94616499eaa8e53f4b1becec81877db9acdb
MaxMillington <maxmillington89@gmail.com>
2015-09-28 14:51:59 -0600

    removed the rufus gem from gemfile. Removed uncecessary require twilio command from notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,4 @@
-require 'twilio-ruby'
+# require 'twilio-ruby'
 
 class NotificationsController < ApplicationController
 
@@ -6,134 +6,37 @@ class NotificationsController < ApplicationController
 
   def monday
     Notify.monday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.monday}" unless location.monday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #     client.messages.create(from: "15702854146",
-    #                            to: "1#{number.phone_number.delete '-'}",
-    #                            body: "#{message.join(', ')}")
-    # end
+
     redirect_to admin_dashboard_path
   end
 
   def tuesday
     Notify.tuesday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.tuesday}" unless location.tuesday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #   client.messages.create(from: "15702854146",
-    #                          to: "1#{number.phone_number.delete '-'}",
-    #                          body: "#{message.join(', ')}")
-    # end
     redirect_to admin_dashboard_path
   end
 
   def wednesday
     Notify.wednesday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.wednesday}" unless location.wednesday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #   client.messages.create(from: "15702854146",
-    #                          to: "1#{number.phone_number.delete '-'}",
-    #                          body: "#{message.join(', ')}")
-    # end
     redirect_to admin_dashboard_path
   end
 
   def thursday
     Notify.thursday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.thursday}" unless location.thursday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #   client.messages.create(from: "15702854146",
-    #                          to: "1#{number.phone_number.delete '-'}",
-    #                          body: "#{message.join(', ')}")
-    # end
     redirect_to admin_dashboard_path
   end
 
   def friday
     Notify.friday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.friday}" unless location.friday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #   client.messages.create(from: "15702854146",
-    #                          to: "1#{number.phone_number.delete '-'}",
-    #                          body: "#{message.join(', ')}")
-    # end
     redirect_to admin_dashboard_path
   end
 
   def saturday
     Notify.saturday
-    # @numbers = Number.all
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-    #                                   ENV["twilio_auth_token"])
-    # @locations = Location.all
-    #
-    # message = @locations.map do |location|
-    #   "#{location.name}: #{location.saturday}" unless location.saturday.nil?
-    # end.compact
-    #
-    # @numbers.each do |number|
-    #   client.messages.create(from: "15702854146",
-    #                          to: "1#{number.phone_number.delete '-'}",
-    #                          body: "#{message.join(', ')}")
-    # end
     redirect_to admin_dashboard_path
   end
 
   def sunday
     Notify.sunday
-      # @numbers = Number.all
-      # client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-      #                                   ENV["twilio_auth_token"])
-      # @locations = Location.all
-      #
-      # message = @locations.map do |location|
-      #   "#{location.name}: #{location.sunday}" unless location.sunday.nil?
-      # end.compact
-      #
-      # @numbers.each do |number|
-      #   client.messages.create(from: "15702854146",
-      #                          to: "1#{number.phone_number.delete '-'}",
-      #                          body: "#{message.join(', ')}")
-      # end
     redirect_to admin_dashboard_path
   end
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,50 +1,29 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
 
 every :monday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.monday"
 end
 
 every :tuesday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.tuesday"
 end
 
 every :wednesday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.wednesday"
 end
 
 every :thursday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.thursday"
 end
 
 every :friday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.friday"
 end
 
 every :saturday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.saturday"
 end
 
 every :sunday, :at => '7am' do
-  runner "Task.do_something_great"
+  runner "Notify.sunday"
 end
 
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
