c2e0ebcd799b7d12c99f11fb32c7c8c2972da9dd
MaxMillington <maxmillington89@gmail.com>
2015-09-29 14:09:53 -0600

    mega-refactoring of the notify.rb file

--- app/services/notify.rb
+++ app/services/notify.rb
@@ -1,128 +1,65 @@
 class Notify
 
-  def self.monday
+  def self.day_check
     @numbers = Number.all
     client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
                                       ENV["twilio_auth_token"])
     @locations = Location.all
+    
+    if Time.now.monday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.monday}" unless location.monday.nil?
+      end.compact
 
-    message = @locations.map do |location|
-      "#{location.name}: #{location.monday}" unless location.monday.nil?
-    end.compact
+      send_message(message, client)
+    elsif Time.now.tuesday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.tuesday}" unless location.tuesday.nil?
+      end.compact
 
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-    end
-  end
+      send_message(message, client)
+    elsif Time.now.wednesday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.wednesday}" unless location.wednesday.nil?
+      end.compact
 
-  def self.tuesday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
+      send_message(message, client)
+    elsif Time.now.thursday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.thursday}" unless location.thursday.nil?
+      end.compact
 
-    message = @locations.map do |location|
-      "#{location.name}: #{location.tuesday}" unless location.tuesday.nil?
-    end.compact
+      send_message(message, client)
+    elsif Time.now.friday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.friday}" unless location.friday.nil?
+      end.compact
 
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-    end
+      send_message(message, client)
+    elsif Time.now.saturday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.saturday}" unless location.saturday.nil?
+      end.compact
 
+      send_message(message, client)
+    elsif Time.now.sunday?
+      message = @locations.map do |location|
+        "#{location.name}: #{location.sunday}" unless location.sunday.nil?
+      end.compact
 
-  end
-
-  def self.wednesday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
-
-    message = @locations.map do |location|
-      "#{location.name}: #{location.wednesday}" unless location.wednesday.nil?
-    end.compact
-
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-    end
-  end
-
-  def self.thursday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
-
-    message = @locations.map do |location|
-      "#{location.name}: #{location.thursday}" unless location.thursday.nil?
-    end.compact
-
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-      end
-  end
-
-  def self.friday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
-
-    message = @locations.map do |location|
-      "#{location.name}: #{location.friday}" unless location.friday.nil?
-    end.compact
-
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-    end
-  end
-
-  def self.saturday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
-
-    message = @locations.map do |location|
-      "#{location.name}: #{location.saturday}" unless location.saturday.nil?
-    end.compact
-
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
-    end
-  end
-
-  def self.sunday
-    @numbers = Number.all
-    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                      ENV["twilio_auth_token"])
-    @locations = Location.all
-
-    message = @locations.map do |location|
-      "#{location.name}: #{location.sunday}" unless location.sunday.nil?
-    end.compact
-
-    @numbers.each do |number|
-      client.messages.create(from: "15702854146",
-                             to: "#{number.phone_number.delete '-, +'}",
-                             body: "#{message.join(', ')}")
+      send_message(message, client)
     end
   end
 
   private
 
+  def self.send_message(message, client)
+    @numbers.each do |number|
+      client.messages.create(from: "15702854146",
+                             to: "#{number.phone_number.delete '-, +'}",
+                             body: "#{message.join(', ')}")
+    end
+  end
 
 end
 
