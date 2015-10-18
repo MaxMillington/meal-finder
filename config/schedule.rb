f315ef135848664d73133c07ba54214e5f6a6e21
MaxMillington <maxmillington89@gmail.com>
2015-10-18 12:47:15 -0600

    removed scheduler but put the code in the schedule.rb file commented out in case I want to start this app up again

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,3 +6,8 @@ every 1.day, :at => '7am' do
 every 1.day, :at => '7am' do
   runner "Number.create_from_sms"
 end
+
+
+#put this in the heroku scheuler to get this app running :
+
+# rails runner "Notify.day_check" && rails runner "Number.create_from_sms"
